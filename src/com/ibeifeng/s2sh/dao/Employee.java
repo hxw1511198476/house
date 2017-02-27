fc113183a1d67f07365f547aaf716d02e3a9a127
steptobe <670974983@qq.com>
2017-02-27 10:17:30 +0800

    dao

---
+++ src/com/ibeifeng/s2sh/dao/Employee.java
@@ -0,0 +1,14 @@
+package com.ibeifeng.s2sh.dao;
+
+/**
+ * Created by 没密码 on 2017/2/18.
+ */
+public class Employee {
+    private int id;
+    private String name;
+
+
+    public void add(){
+
+    }
+}
